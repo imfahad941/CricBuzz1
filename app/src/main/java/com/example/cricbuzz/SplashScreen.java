6323748821f3a741ae8492d0839397fb34518ec6
fahad <mohdfahad941@gmail.com>
2023-04-30 15:31:30 +0530

    second

--- app/src/main/java/com/example/cricbuzz/SplashScreen.java
+++ app/src/main/java/com/example/cricbuzz/SplashScreen.java
@@ -2,9 +2,13 @@ package com.example.cricbuzz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -12,6 +16,9 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) ImageView imageView=findViewById(R.id.img);
+
+        Glide.with(this).asGif().load(R.raw.cricket).into(imageView);
 
 
         new Handler().postDelayed(new Runnable() {
